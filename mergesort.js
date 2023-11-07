67eb56c59889fca5c8c9f87697e49f6f670cb4cd
Isaac Munene Ndereba <38168136+munenendereba@users.noreply.github.com>
2023-11-07 12:15:03 +0300

    added merge

--- mergesort.js
+++ mergesort.js
@@ -1,15 +1,30 @@
 const mergeSort = (startArray) => {
   const length = startArray.length;
   if (length === 1) {
-    console.log(startArray);
     return startArray;
   }
+
   const mid = Math.floor(length / 2);
   const leftArray = startArray.slice(0, mid);
   const rightArray = startArray.slice(mid, length);
 
-  mergeSort(leftArray);
-  mergeSort(rightArray);
+  return merge(mergeSort(leftArray), mergeSort(rightArray));
+};
+
+const merge = (leftArray, rightArray) => {
+  const sortedArray = [];
+
+  while (leftArray.length > 0 && rightArray.length > 0) {
+    if (leftArray[0] < rightArray[0]) {
+      sortedArray.push(leftArray[0]);
+      leftArray.splice(0, 1);
+    } else {
+      sortedArray.push(rightArray[0]);
+      rightArray.splice(0, 1);
+    }
+  }
+
+  return sortedArray.concat(leftArray, rightArray);
 };
 
 const inputArr = [3, 5, 2, 90, 4, 7];
